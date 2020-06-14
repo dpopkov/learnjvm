e6efbe0a622a2fba2e45c58dacab289a01a93ee0
Denis Popkov <pkvdenis@gmail.com>
2020-06-14 18:57:14 +0300

    ujvmmm: add commons-cli to WeakHashMapUsage

--- ujvmmm/src/main/java/learn/jvm/ujvmmm/m04refs/WeakHashMapUsage.java
+++ ujvmmm/src/main/java/learn/jvm/ujvmmm/m04refs/WeakHashMapUsage.java
@@ -1,10 +1,11 @@
 package learn.jvm.ujvmmm.m04refs;
 
 import java.util.WeakHashMap;
+import org.apache.commons.cli.*;
 
 public class WeakHashMapUsage {
-    @SuppressWarnings("UnusedAssignment")
     public static void main(String[] args) {
+        CommandLine cmd = prepareArgs(args);
         WeakHashMap<Person, PersonMetaData> weakHashMap = new WeakHashMap<>();
         Person kevin = new Person();
         weakHashMap.put(kevin, new PersonMetaData());
@@ -12,18 +13,39 @@ public class WeakHashMapUsage {
         PersonMetaData metaData = weakHashMap.get(kevin);
         System.out.println(metaData);
 
+        //noinspection UnusedAssignment
         kevin = null;
-        System.gc();
+        if (cmd.hasOption("GC")) {
+            System.gc();
+        }
 
-        try {
-            Thread.sleep(1L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (cmd.hasOption("w")) {
+            try {
+                Thread.sleep(1L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         if (weakHashMap.containsValue(metaData)) {
             System.out.println("Still contains key");
         } else {
             System.out.println("Key gone");
         }
+    }
+
+    private static CommandLine prepareArgs(String[] args) {
+        Options options = new Options();
+        options.addOption("GC", false, "garbage collection");
+        options.addOption("w", false, "wait for garbage collection");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            new HelpFormatter().printHelp("java " + WeakHashMapUsage.class.getName(), options, true);
+            System.exit(1);
+        }
+        return cmd;
     }
 }
